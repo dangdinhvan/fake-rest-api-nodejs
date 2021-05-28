3080030c3035975f7781a72756932d856cef0400
dangdinhvan <untilyou1235@gmail.com>
2021-05-28 13:15:08 +0700

    chinh sua final round

--- public/chinh-sua-hoc-vien.js
+++ public/chinh-sua-hoc-vien.js
@@ -38,7 +38,7 @@ $('#save-btn').click(function () {
             dataType: 'json'
         }).done(function () {
             alert('Cập nhập thông tin thành công');
-            window.location("https://create-server-by-van.herokuapp.com");
+            window.location.href("https://create-server-by-van.herokuapp.com");
         });
     }
 });
