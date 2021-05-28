0d4febc4431136534bb219c6cc6711f09380f534
dangdinhvan <79373061+dangdinhvan@users.noreply.github.com>
2021-05-28 13:19:53 +0700

    Update chinh-sua-hoc-vien.js

--- public/chinh-sua-hoc-vien.js
+++ public/chinh-sua-hoc-vien.js
@@ -38,7 +38,7 @@ $('#save-btn').click(function () {
             dataType: 'json'
         }).done(function () {
             alert('Cập nhập thông tin thành công');
-            window.location.href("https://create-server-by-van.herokuapp.com");
+            location.href("https://create-server-by-van.herokuapp.com");
         });
     }
 });
