d5b068c460c7a4f4bf710a186b78b3708dc58946
dangdinhvan <untilyou1235@gmail.com>
2021-06-13 19:17:23 +0700

    update search and paginate

--- public/danh-sach-hoc-vien.js
+++ public/danh-sach-hoc-vien.js
@@ -1,38 +1,5 @@
 const DOMAIN = "https://create-server-by-van.herokuapp.com";
-// Hàm chức năng vẽ giao diện và nội dung
-function loadDocJQuery() {
-  $.ajax(DOMAIN + "/users", {
-    method: "GET"
-  }).done(function (users) {
-    let content = "";
-
-    for (let i = 0; i < users.length; i++) {
-      const user = users[i];
-
-      content += `<tr id="${user.id}">
-                    <td>${user.firstName} ${user.lastName}</td>
-                    <td>${user.birthday}</td>
-                    <td>${user.email}</td>
-                    <td>${user.phone}
-                    <span>
-                      <a class="edit-btn" href="./chinh-sua-hoc-vien.html?id=${user.id}")"><i class="fas fa-edit icon"></i>Chỉnh
-                        sửa</a>
-                      | 
-                      <button class="del-btn" onclick="del(${user.id})" data-bs-toggle="modal" data-bs-target="#exampleModal"><i
-                            class="fas fa-trash-alt icon"></i>Xóa</button>
-                    </span>
-        </td>
-    </tr>`;
-    };
-
-    $("#table-users").html(content);
-  });
-};
-
-$(function () {
-  loadDocJQuery();
-});
-//-----------------------------
+const limitOnePage = 2;
 
 // Hàm chức năng xóa
 function del(id) {
@@ -50,3 +17,126 @@ function del(id) {
   });
 }
 //-----------------------------
+
+// Hàm chức năng tìm kiếm
+function search() {
+  if ($('#search-box').val() === "") {
+    $("#table-users").html('');
+  }
+  else {
+    $.ajax(DOMAIN + `/users?q=${$('#search-box').val()}`, {
+      method: "GET"
+    }).done(function (users) {
+      let content = "";
+      for (let i = 0; i < users.length; i++) {
+        const user = users[i];
+        content += `<tr id="${user.id}">
+                    <td>${user.firstName} ${user.lastName}</td>
+                    <td>${user.birthday}</td>
+                    <td>${user.email}</td>
+                    <td>${user.phone}</td>
+                    <td>
+                        <span id="edit-del-btn">
+                        <a class="edit-btn" href="./chinh-sua-hoc-vien.html?id=${user.id}")"><i class="fas fa-edit icon"></i>Chỉnh
+                        sửa</a>
+                        | 
+                        <button class="del-btn" onclick="del(${user.id})" data-bs-toggle="modal" data-bs-target="#exampleModal"><i
+                            class="fas fa-trash-alt icon"></i>Xóa</button>
+                        </span>
+                    </td>
+                  </tr>`;
+      }
+      $("#table-users").html(content);
+    });
+  }
+}
+
+
+
+$("#search-box").keyup(function (event) {
+  if (event.keyCode === 13) {
+    search();
+  }
+});
+//------------------------------
+
+// Hàm chức năng phân trang
+function paginate() {
+  $.ajax(DOMAIN + '/users?_page=1&_limit=' + limitOnePage + "&_sort=id&_order=desc", {
+    method: "GET"
+  }).done(function (users, textStatus, request) {
+    let totalPage = Math.ceil(request.getResponseHeader('x-Total-Count') / limitOnePage);
+    let content = "";
+    let contentPagination = "";
+
+    for (let i = 0; i < users.length; i++) {
+      const user = users[i];
+
+      content += `<tr id="${user.id}">
+                    <td>${user.firstName} ${user.lastName}</td>
+                    <td>${user.birthday}</td>
+                    <td>${user.email}</td>
+                    <td>${user.phone}</td>
+                    <td>
+                        <span id="edit-del-btn">
+                        <a class="edit-btn" href="./chinh-sua-hoc-vien.html?id=${user.id}")"><i class="fas fa-edit icon"></i>Chỉnh
+                        sửa</a>
+                        | 
+                        <button class="del-btn" onclick="del(${user.id})" data-bs-toggle="modal" data-bs-target="#exampleModal"><i
+                            class="fas fa-trash-alt icon"></i>Xóa</button>
+                        </span>
+                    </td>
+                  </tr>`;
+    };
+    $("#table-users").html(content);
+
+    for (let i = 1; i <= totalPage; i++) {
+      contentPagination += `<button class="paginate-btn btn-${i}" onclick="pagiNation(${i},${totalPage})">${i}</button>`
+    }
+    $('#paginate-box').html(contentPagination);
+    $(`.btn-1`).css('background-color', 'black').css('color','white');
+  })
+}
+paginate();
+
+//-----------------------------------------
+
+// Bấm vào số trang để hiện trang tương ứng
+function pagiNation(pageNumber, totalPage) {
+  $.ajax(DOMAIN + `/users?_page=${pageNumber}&_limit=` + limitOnePage + "&_sort=id&_order=desc", {
+    method: "GET"
+  }).done(function (users) {
+    let content = "";
+    for (let i = 0; i < users.length; i++) {
+      const user = users[i];
+      content += `<tr id="${user.id}">
+                    <td>${user.firstName} ${user.lastName}</td>
+                    <td>${user.birthday}</td>
+                    <td>${user.email}</td>
+                    <td>${user.phone}</td>
+                    <td>
+                        <span id="edit-del-btn">
+                        <a class="edit-btn" href="./chinh-sua-hoc-vien.html?id=${user.id}")"><i class="fas fa-edit icon"></i>Chỉnh
+                        sửa</a>
+                        | 
+                        <button class="del-btn" onclick="del(${user.id})" data-bs-toggle="modal" data-bs-target="#exampleModal"><i
+                            class="fas fa-trash-alt icon"></i>Xóa</button>
+                        </span>
+                    </td>
+                  </tr>`;
+    }
+    $("#table-users").html(content);
+    $(`.btn-${pageNumber}`).css('background-color', 'black').css('color','white');
+    for(let i=1;i<=totalPage;i++){
+      if(i!==pageNumber){
+        $(`.btn-${i}`).css('background-color', 'white').css('color','black');
+      }
+    }
+  });
+}
+//-----------------------------------
+
+// Hàm Sort
+function sort() {
+}
+//--------------------------------------
