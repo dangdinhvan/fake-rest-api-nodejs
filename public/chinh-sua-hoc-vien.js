7e428c58ff730b89bf42252ba782c8514c11783b
dangdinhvan <untilyou1235@gmail.com>
2021-10-08 22:19:21 +0700

    fix v5

--- public/chinh-sua-hoc-vien.js
+++ public/chinh-sua-hoc-vien.js
@@ -65,24 +65,28 @@ $("#save-btn").click(function () {
     $("#firstName").val() !== "" &&
     $("#lastName").val() !== "" &&
     $("#birthday").val() !== "" &&
-    $("#phone").val() !== "" &&
-    validateEmail() === true
+    $("#email").val() !== "" &&
+    $("#phone").val() !== ""
   ) {
-    $.ajax({
-      url: `https://create-server-by-van.herokuapp.com/users/${id}`,
-      type: "PATCH",
-      data: {
-        firstName: $("#firstName").val(),
-        lastName: $("#lastName").val(),
-        birthday: $("#birthday").val(),
-        email: $("#email").val(),
-        phone: $("#phone").val(),
-      },
-      dataType: "json",
-    }).done(function () {
-      alert("Cập nhập thông tin thành công");
-      location.href = "/danh-sach-hoc-vien.html";
-    });
+    if (validateEmail() === true) {
+      $.ajax({
+        url: `https://create-server-by-van.herokuapp.com/users/${id}`,
+        type: "PATCH",
+        data: {
+          firstName: $("#firstName").val(),
+          lastName: $("#lastName").val(),
+          birthday: $("#birthday").val(),
+          email: $("#email").val(),
+          phone: $("#phone").val(),
+        },
+        dataType: "json",
+      }).done(function () {
+        alert("Cập nhập thông tin thành công");
+        location.href = "/danh-sach-hoc-vien.html";
+      });
+    } else {
+      alert('email phải bao gồm dấu "."');
+    }
   }
 });
 //------------------------------------------
